1b3705c9db66255b23009bba4ee17dc976685879
Deepakjhajhria <deepakjhajhria4657@gmail.com>
2023-09-21 10:25:00 +0530

    modify

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,11 @@ let span1 = document.querySelector(".span1")
 let span2 = document.querySelector(".span2")
 let span3 = document.querySelector(".span3")
 let home = document.querySelector(".home")
+let misson = document.querySelector(".misson")
+let training = document.querySelector(".training")
+let history = document.querySelector(".history")
+let forum = document.querySelector(".forum")
+let exhibition = document.querySelector(".exhibition")
 
 menu.addEventListener("click", function () {
     mobileView.classList.toggle("show");
@@ -14,6 +19,41 @@ menu.addEventListener("click", function () {
     span3.classList.toggle("span6");
 })
 home.addEventListener("click", function () {
+    mobileView.classList.remove("show");
+    body.classList.remove("overflow-hidden");
+    span1.classList.remove("span4");
+    span2.classList.remove("span5");
+    span3.classList.remove("span6");
+})
+misson.addEventListener("click", function () {
+    mobileView.classList.remove("show");
+    body.classList.remove("overflow-hidden");
+    span1.classList.remove("span4");
+    span2.classList.remove("span5");
+    span3.classList.remove("span6");
+})
+training.addEventListener("click", function () {
+    mobileView.classList.remove("show");
+    body.classList.remove("overflow-hidden");
+    span1.classList.remove("span4");
+    span2.classList.remove("span5");
+    span3.classList.remove("span6");
+})
+history.addEventListener("click", function () {
+    mobileView.classList.remove("show");
+    body.classList.remove("overflow-hidden");
+    span1.classList.remove("span4");
+    span2.classList.remove("span5");
+    span3.classList.remove("span6");
+})
+forum.addEventListener("click", function () {
+    mobileView.classList.remove("show");
+    body.classList.remove("overflow-hidden");
+    span1.classList.remove("span4");
+    span2.classList.remove("span5");
+    span3.classList.remove("span6");
+})
+exhibition.addEventListener("click", function () {
     mobileView.classList.remove("show");
     body.classList.remove("overflow-hidden");
     span1.classList.remove("span4");
