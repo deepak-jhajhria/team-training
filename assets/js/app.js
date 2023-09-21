e4f93f3f65a4e335bb1514c242dc607758c73a06
Deepakjhajhria <deepakjhajhria4657@gmail.com>
2023-09-21 09:38:39 +0530

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,11 +4,19 @@ let body = document.querySelector("body");
 let span1 = document.querySelector(".span1")
 let span2 = document.querySelector(".span2")
 let span3 = document.querySelector(".span3")
+let home = document.querySelector(".home")
 
 menu.addEventListener("click", function () {
     mobileView.classList.toggle("show");
-    body.classList.toggle("ovrflowH");
+    body.classList.toggle("overflow-hidden");
     span1.classList.toggle("span4");
     span2.classList.toggle("span5");
     span3.classList.toggle("span6");
+})
+home.addEventListener("click", function () {
+    mobileView.classList.remove("show");
+    body.classList.remove("overflow-hidden");
+    span1.classList.remove("span4");
+    span2.classList.remove("span5");
+    span3.classList.remove("span6");
 })
